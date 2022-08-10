0edf579c7f3ee626310c87fe2e95a58901e90984
ttpn18121996 <ttpn18121996@gmail.com>
2022-08-11 04:01:58 +0700

    Install tailwind and sass

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/scss/app.scss',
+                'resources/scss/auth.scss',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
+        tailwindcss()
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        }
+    }
 });
